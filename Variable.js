6bf40c9dada760737b39b602e4cb029fdcf53858
ahmedaliqureshi <ahmedaliqureshi59@gmail.com>
2025-10-29 11:04:17 +0000

    variables in JS

---
+++ Variable.js
@@ -0,0 +1,27 @@
+// variable are kind of container,in which you can keep values in it and take it, and they are declared by the let keyword
+
+// when the variable is decalred it should be follwed by the equal (assaignment operator) to vlaue or string 
+
+let numb='number'; 
+
+//to print the vlaues which is stored in the variable, just write the consoel log then follow it up with the name of the variable
+
+console.log(numb);
+
+// the value of the variable can be changed later on as well, but it won't be needing let keyword to decalre it
+
+console.log('text stored in the variable was number');
+
+numb='number2'; 
+
+console.log(numb);
+
+console.log('\t the text stored in the same variable is number2');
+
+// the if we store number instead of the text, then that is called constant, and constent has another key word for it, and constent should be given a name as well, similar to variable but it only deals with numbers
+
+const age= 10; 
+console.log('the const cannot be change once it is declared and it should be given name as well')
+console.log(age);
+
+
