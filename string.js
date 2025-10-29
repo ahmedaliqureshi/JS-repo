ebfaa8c3651e7624304fc6a85417704c874b0053
ahmedaliqureshi <ahmedaliqureshi59@gmail.com>
2025-10-29 10:12:49 +0000

    double and single quotation to display text in JS

---
+++ string.js
@@ -0,0 +1,12 @@
+// in this file we are going to see how to declare the string and what are the different ways to show string 
+
+// 1st method:- is by the use of ("") i.e double quotation
+
+console.log("we can declare the text in JS with double quotation");
+
+// 2nd method: is by the use of ('') i.e single quotation
+
+console.log('we can declare the text in JS with single quoation as well'); 
+
+// adding the semi colon at the end of the code is good practice
+
