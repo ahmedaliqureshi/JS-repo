f56a6b8c9b4fa83ba2a8bda993f518ba69ac76bb
ahmedaliqureshi <ahmedaliqureshi59@gmail.com>
2025-10-29 12:19:43 +0000

    arithematic operators

---
+++ Arithematicops.js
@@ -0,0 +1,16 @@
+// arithematic operators are +.-.* and /, this operator help us in calulating 
+
+let a=2; 
+let b=3; 
+// for addition 
+let c = a+b; 
+console.log(c);
+// for substraction
+c=a-b 
+console.log(c); 
+// for multiplication
+c=a*b;
+console.log(c); 
+// for division 
+c=a/b; 
+console.log(c); 
