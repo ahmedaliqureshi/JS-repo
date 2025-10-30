42f847956a7340b55a755053efb276ef41bb2ac4
ahmedaliqureshi <ahmedaliqureshi59@gmail.com>
2025-10-30 09:23:55 +0000

    decalring variable as function

---
+++ Varfunction.js
@@ -0,0 +1,11 @@
+// isntaed of calling the functio we can store the function in a varibale, and call it rather then 
+// calling the fucntion it self
+
+let variable = function(a,b)
+{
+    let name=("xyz"); 
+    a+b; 
+    console.log(`my name is ${name}, and i push around ${a+b} of code to github everyday`); 
+}
+variable(10,20); 
+
