9d3ce9aefe36b6e5390056f325a81edd08f678e6
ahmedaliqureshi <ahmedaliqureshi59@gmail.com>
2025-10-30 09:07:55 +0000

    function with parameters

---
+++ Functionvar.js
@@ -0,0 +1,25 @@
+// functions can also contian variables in parathesis 
+// which we call them parameter
+// this parameters are used to pass vlaue to funciton 
+
+function add(a,b)
+{
+    a+b; 
+    console.log(a+b);
+}
+add(10,20); 
+
+function sub(c,d)
+{
+    c-d; 
+    console.log(c-d); 
+}
+sub(40,50);
+
+function mul(e,f)
+{
+    e*f; 
+    console.log(e*f); 
+}
+mul(200,300); 
+
