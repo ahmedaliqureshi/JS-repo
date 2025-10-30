1f9d4bcbb710bb8d47bf8c9be585fa92a6fa7693
ahmedaliqureshi <ahmedaliqureshi59@gmail.com>
2025-10-30 08:57:28 +0000

    simpel function program

---
+++ Assignmentops.js
@@ -0,0 +1,12 @@
+// assigment operators help us to assigna the values to variables or perform the arithematic operations with the value. 
+// few of the assignment operator are that we use commonly are  =, ==, ===, !=, >,<, >=,<= 
+
+
+// 1st let's decalre an variable and assign a value to 
+// then we can perform the operations to it
+
+let numb=22;
+console.log(`here we have taken ${numb}, and perform the += operation on it, which is same as x= x+n`);
+numb+=5
+console.log(`${numb}`);
+
---
+++ functions.js
@@ -0,0 +1,13 @@
+// funcion has is an independent part of code, that can 
+// be executed by it self, this is done when we call
+// the name of the function or call the variable 
+// and you can reuse it when ever you want
+// it get declared with the keyword function
+
+function callme()
+{
+    console.log('hello this is user, i am writing this message to call the fucntion and respond');
+}
+
+callme(); 
+
